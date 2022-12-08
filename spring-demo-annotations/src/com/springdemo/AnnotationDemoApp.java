5108d235c2cb1b846eaab418b5761da633c6fa1c
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-12-09 02:21:13 +0530

    setter injection

--- spring-demo-annotations/src/com/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/springdemo/AnnotationDemoApp.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 *   - special labels/markers added to java classes
 *   - provide meta-data about the class
 *   - processed at compile time or run-time for special processing
-*          ex - @Override
+*          example - @Override
 *
 *
 * why spring configuration with Annotation 
@@ -18,14 +18,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 * once you added Annotations to a class then spring automatically scan your java classes and when it finds the class that has special spring annoation on it. it will automatically register bean with the container.
 *
 * Development process -
-*   - Enable component scanning in spring config file
-*   - Add the @Component annoation to your java classes
+*   - Enable component scanning in spring configuration file
+*   - Add the @Component annotation to your java classes
 *   - Retrieve bean from spring container
 *
 *
 * Default Bean ID - spring also support defaults bean ID
 *                 - the class name, make first letter lower-case.
-*                 - @Component - dont need to provide ID within ("")
+*                 - @Component - don't need to provide ID within ("")
 */
 
 public class AnnotationDemoApp {
@@ -40,6 +40,9 @@ public class AnnotationDemoApp {
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		//call method to get the daily fortune
+		System.out.println(theCoach.getDailyFortune());
 		
 		//close the context
 		context.close();
--- spring-demo-annotations/src/com/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/springdemo/TennisCoach.java
@@ -1,15 +1,52 @@
 package com.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach")
 
 @Component // Default Bean ID ( tennisCoach )
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	/*
+	 * Spring will scan for a component that implements FortuneService interface.
+	 * 
+	 * 1. what if there are multiple FortuneService interface?
+	 *    - Spring has support to handle this.  
+	 */
+	
+	//Setter injection
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	/*
+	constructor dependency injection using spring annotation
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	/*
+	auto-wired using any method not just setter or constructor injection
+	@Autowired
+	public void doSomeCrazzyStuff(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
