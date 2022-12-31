f71c203fdd086a3e0ecc005234ad3dcc83959587
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-12-31 20:03:39 +0530

    metadata

--- oops/src/collection/MapLearn.java
+++ oops/src/collection/MapLearn.java
@@ -1,4 +1,16 @@
 package collection;
+/*
+ *  map - A map contains values on the basis of key
+ *      - Each key and value pair known as entry
+ *      
+ *  - There are two interface for implementing map 
+ *     1. Map
+ *     2. Sorted Map     
+ *  - There are three classes extending this interface
+ *     1. HashMap
+ *     2. LinkedHashMap
+ *     3. TreeMap
+ */
 
 public class MapLearn {
 
