cefc1d7c4af7b7cbd55dcbb5b401f8eefb2cbd3f
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2023-01-04 23:31:03 +0530

    configured Resource Bundle

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -28,9 +28,10 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@NotNull(message="is required")
 	@Min(value = 0, message="value must be equal to or greater than 0")
 	@Max(value = 10, message="value must be smaller than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits allowed")
 	private String postalCode;
@@ -51,11 +52,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
